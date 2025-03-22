33ff3ed44cdfb3e5ef1577fc2cf20a4391cdc84a
Raphael Hagel <hertzpatrick13@gmail.com>
2025-03-22 15:15:42 +0100

    Update BUILD.bat

--- BUILD.bat
+++ BUILD.bat
@@ -10,7 +10,14 @@ pyinstaller ^
   --onefile ^
   --noconsole ^
   --icon "data\icon.ico" ^
-  --add-data "data;data" ^
+  --add-data "data\icon.ico;." ^
+  --add-data "data\ProtonVPN.zip;." ^
+  --add-data "data\jetbrains_toolbox.zip;." ^
+  --add-data "data\brave.zip;." ^
+  --add-data "data\brave_32x32.png;." ^
+  --add-data "data\adobe_normal.cur;." ^
+  --add-data "data\adobe_click.cur;." ^
+  --add-data "data\ublock.xpi;." ^
   EAIT-Tool.py
 
 echo.
